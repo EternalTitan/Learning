d84a028dfb2e1d1077f4deb116f35958658b877f
Simon <s@simon0.com>
2017-12-06 15:17:13 -0500

    finish project7 pthread debug, the materials from CMU is incorrect!

--- project7.c
+++ project7.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
 
 long long int total_number_of_tosses;	//Total number of tosses
+long long int average_number_of_tosses;
 long long int number_in_circle = 0; //Each thread is adding its result to this variable.
 int thread_count; //Number of threads.
-pthread_mutex_t hitNum_mutex; //To guard the share variable.
+pthread_mutex_t hitNum_mutex= PTHREAD_MUTEX_INITIALIZER; //To guard the share variable.
 
 void *thread_toss(void* rank); //Each thread calls this function.
+int DEBUG = 0;
+
 
 int main()
 {
@@ -19,18 +23,31 @@ int main()
     //Using the following loop to make it divisible by thread_count.
     while (total_number_of_tosses % thread_count != 0) total_number_of_tosses++;
     //Complete the rest of this function.
-    thread_handles = malloc(thread_count* sizeof(pthread_t));
-    long long int average_number_of_tosses = total_number_of_tosses / thread_count;
-    hitNum_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    average_number_of_tosses = total_number_of_tosses / (long long int)thread_count;
+    thread_handles = malloc((thread_count+3)* sizeof(pthread_t));
+
+    if (DEBUG)
+    printf("anof: %lld",average_number_of_tosses);
     for (thread=0;thread<thread_count;thread++)
     {
-        pthread_create(&thread_handles[thread],NULL,thread_toss(),(void*) average_number_of_tosses);
+        if (DEBUG)
+            printf("helllo\n");
+        pthread_create(&thread_handles[thread],NULL,thread_toss,(void*) thread);
     }
+
+    if (DEBUG)
+        printf("join\n");
     for (thread=0;thread<thread_count;thread++)
     {
+        if (DEBUG)
+            printf("join1111\n");
         pthread_join(thread_handles[thread],NULL);
     }
-    pi_estimate = 4 * number_in_circle / ((double)number_of_tosses);
+    if (DEBUG)
+        printf("number_in_circle:%lld  total_number_of_tosses:%lld\n",number_in_circle,total_number_of_tosses);
+
+    pi_estimate = 4 * number_in_circle / ((double)total_number_of_tosses);
     printf("Estimated pi = %f\n", pi_estimate);
     return 0;
 }
@@ -42,11 +59,14 @@ double rand_double() {
 
 void *thread_toss(void* rank) {
     //Complete this function
-    long long int *data;
     long long int i,tot=0;
     double x,y,distance_squared;
-    data = (long long int*) rank;
-    for (i = 0; i < data; ++i) {
+    if (DEBUG)
+        printf("tttttttttt\n");
+    if (DEBUG)
+        printf("average_number_of_tosses:%lld\n",average_number_of_tosses);
+
+    for (i = 0; i < average_number_of_tosses; ++i) {
         x = rand_double();
         y = rand_double();
         distance_squared = x*x + y*y;
@@ -57,5 +77,5 @@ void *thread_toss(void* rank) {
     number_in_circle=number_in_circle+tot;
     pthread_mutex_unlock( &hitNum_mutex );
 
-    return NULL;
+    pthread_exit(NULL);
 }
